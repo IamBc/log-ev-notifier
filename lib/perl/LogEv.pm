470ac4b3ea58b344f606d8bb7df61c45c24caa7d
Bloodcount <searosss@gmail.com>
2016-05-24 00:15:09 +0300

    feat: module structure

--- lib/perl/LogEv.pm
+++ lib/perl/LogEv.pm
@@ -8,25 +8,29 @@ use File::Tail;
 use Getopt::Long;
 
 
-
 sub WatchLog()
 {
-    my $options = GetConfig();
-    !defined $options ? die("System error!") : 1;
+    my $options = {};
+    GetOptions("log_name=s" => \$$options{log_name}) or die("Error in command line arguments\n"); 
     
     if(!defined $$options{log_name})
     {
         die();
     }
-
 }
 
 
-sub GetConfig()
+sub FilterLog($)
 {
-    my $options = {};
-    GetOptions("log_name=s" => \$$options{log_name}) or die("Error in command line arguments\n"); 
-    return $options;
+    my ($options) = @_;
+    
+    !defined $options ? die("System error!", '001') : 1;
+    my $file = File::Tail->new(name=>$$options{log_name}, maxinterval=>3, adjustafter=>7);
+    while (defined(my $line = $file->read)) 
+    {
+        print "$line";
+    }
+
 }
 
 1;
